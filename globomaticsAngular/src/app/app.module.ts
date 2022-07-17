a57b3cda9bfefc9bcd13422421a0273c8562fc40
mishevski-k <89786242+mishevski-k@users.noreply.github.com>
2022-07-17 20:16:15 +0200

    Added initial project push

---
+++ globomaticsAngular/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HttpClientModule} from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule  }   from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthService } from './services/auth.service';
+
+import { BikeService } from './services/bike.service';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewRegistrationComponent } from './components/view-registration/view-registration.component';
+import { CallbackComponent } from './components/callback/callback.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    ViewRegistrationComponent,
+    CallbackComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    BikeService, 
+    AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
